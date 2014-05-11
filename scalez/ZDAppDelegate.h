d95c6c80bb4b79bc2957a2b6a99a04b249bfae40
jorge moura <jorge.moura@iuz.pt>
2014-05-11 21:26:40 +0100

    Initial Commit

---
+++ scalez/ZDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZDAppDelegate.h
+//  scalez
+//
+//  Created by Jorge Moura on 11/05/14.
+//  Copyright (c) 2014 Jorge Moura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
